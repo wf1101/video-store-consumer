d25a845e9d47ff01d1ae2085badf77832a93ba12
Chris <chris@adadevelopersacademy.org>
2017-06-12 10:01:11 -0700

    Added Backbone-baseline.

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// /src/app.js
+
+// Import jQuery & Underscore
+import $ from 'jquery';
+import _ from 'underscore';
+
+// ready to go
+$(document).ready(function() {
+
+  $('section.main-content').append('<p>Hello World!</p>');
+
+});
