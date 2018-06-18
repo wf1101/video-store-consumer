d1bffdba617fc3a177fac923dee93a2ace22256b
Wenjie <traxiaojie@gmail.com>
2018-06-18 15:38:35 -0700

    Installed react router packages Added routes of search, library and customers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Library from './components/Library';
+import Search from './components/Search';
 import CustomerCollection from'./components/CustomerCollection';
+import { BrowserRouter } from 'react-router-dom';
+import { Route} from 'react-router';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <Library />
-       <CustomerCollection />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				{/* here's a div */}
+				<div>
+					<Route path="/search" component={ Search }/>
+					<Route path="/library" component={ Library }/>
+					<Route path="/customers" component={ CustomerCollection }/>
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Search extends React.Component{
+
+	render(){
+		return(
+			<div>
+				Haha
+			</div>
+		)
+	}
+}
+
+export default Search;
