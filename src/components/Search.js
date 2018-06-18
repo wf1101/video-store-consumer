c8e5b8658e59780c52fe829de7bdb971e687853f
Wenjie <traxiaojie@gmail.com>
2018-06-18 16:45:59 -0700

    Added submit handler to dispaly movie list from search request

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie.js';
 
-
-const {MOVIEDB_KEY} = process.env
-const API_URL = 'https://api.themoviedb.org/3/search/movie?'
+const MOVIEDB_KEY = process.env.REACT_APP_MOVIEDB_KEY;
+const API_URL = 'https://api.themoviedb.org/3/search/movie';
 
 class Search extends React.Component{
-  state = {
-    query: ''
+	state = {
+		query: '',
+		movies: []
 	}
 
 	handleInputChange = (event) => {
@@ -16,12 +17,47 @@ class Search extends React.Component{
 		})
 	}
 
+	listMovies() {
+		axios.get(`${API_URL}?api_key=${MOVIEDB_KEY}&query=${this.state.query}`)
+		.then((response) => {
+			console.log(response.data);
+			this.setState({
+				movies: response.data.results.slice(0, 20)
+			})
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+	}
+
+	onFormSubmit = (event) => {
+		event.preventDefault();
+
+		console.log("event");
+    this.listMovies();
+	}
+
 	render(){
+		const imageUrl = 'https://image.tmdb.org/t/p/w300'
+
+		const movieList = this.state.movies.map((movie, index) => {
+			return <Movie
+				key={ index }
+				title={ movie.title }
+				image_url={ imageUrl + movie.poster_path }
+			/>
+		})
+
 		return(
-			<form>
-				<input placeholder="Find a movie" name="title" value={ this.state.query } onChange={this.handleInputChange}/>
-				<input type="submit" value="Search"/>
-			</form>
+			<div>
+				<form onSubmit={ this.onFormSubmit }>
+					<input placeholder="Find a movie" name="title" value={ this.state.query } onChange={this.handleInputChange}/>
+					<input type="submit" value="Search"/>
+				</form>
+				<section>
+					{ movieList }
+				</section>
+			</div>
 		)
 	}
 }
