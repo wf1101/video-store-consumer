52233f6a813fdc0182eac9f848329f9379dea9ba
Wenjie <traxiaojie@gmail.com>
2018-06-18 16:48:40 -0700

    Added alt for images

--- src/components/CustomerCollection.js
+++ src/components/CustomerCollection.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Customer from './Customer';
 import axios from 'axios';
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,7 +10,7 @@ class Movie extends React.Component{
 	render(){
 		return(
 			<section>
-				<img src={this.props.image_url}/>
+				<img src={this.props.image_url} alt='movie-poster'/>
 				<h2>Name: { this.props.title }</h2>
 			</section>
 		)
