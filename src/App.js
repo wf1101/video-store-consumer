13e03ea14c514c1121ba63719953c4c07ac09fbf
Wenjie <traxiaojie@gmail.com>
2018-06-19 10:14:48 -0700

    Craeted homepage and navagation bar

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import './App.css';
 import Library from './components/Library';
 import Search from './components/Search';
 import CustomerCollection from'./components/CustomerCollection';
-import { BrowserRouter } from 'react-router-dom';
-import { Route} from 'react-router';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
 	render() {
 		return (
-			<BrowserRouter>
-				{/* here's a div */}
+			<Router>
 				<div>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/search">Search</Link>
+						</li>
+						<li>
+							<Link to="/library">Library</Link>
+						</li>
+						<li>
+							<Link to="/customers">Customers</Link>
+						</li>
+					</ul>
 					<Route path="/search" component={ Search }/>
 					<Route path="/library" component={ Library }/>
 					<Route path="/customers" component={ CustomerCollection }/>
 				</div>
-			</BrowserRouter>
+			</Router>
 		);
 	}
 }
